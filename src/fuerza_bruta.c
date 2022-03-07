ce5da6ee8b942c68e083de04be4d8bfc6df6381e
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-07 08:14:53 -0600

    closes #1
    Sigue algoritmo #1, mas algunas condicionales para evitar errores

--- src/fuerza_bruta.c
+++ src/fuerza_bruta.c
@@ -1,1 +1,63 @@
 #include "include/fuerza_bruta.h"
+
+void fuerza_bruta(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
+                  void (*func_dibujar)(int, int, double, double, double))
+{
+
+    long double m, b, yx;
+
+    if (x1 < x0)
+    {
+        int tmpx = x0,
+            tmpy = y0;
+        x0 = x1;
+        y0 = y1;
+        x1 = tmpx;
+        y1 = tmpy;
+    }
+    else if (x0 == x1)
+    {
+        m = (long double)(y1 - y0);
+    }
+    else
+    {
+        m = (long double)(y1 - y0) / (x1 - x0);
+    }
+
+    b = (long double)y0 - m * x0;
+
+
+    if (x0 >= y0 && x0 != x1)
+    {
+        for (int i = x0; i <= x1; i++)
+        {
+            yx = m * i + b;
+            (*func_dibujar)(i, (int)(round(yx)), rc, gc, bc);
+        }
+    }
+    else if (x0 == x1)
+    {
+        for (int i = y0; i <= y1; i++)
+        {
+            (*func_dibujar)(x0, i, rc, gc, bc);
+        }
+    }
+    else
+    {
+        for (int i = y0; i <= y1; i++)
+        {
+            yx = (i - b) / m;
+            (*func_dibujar)((int)(round(yx)), i, rc, gc, bc);
+        }
+    }
+}
+
+
+/**
+ * Casos:
+ * 
+ * fuerza_bruta(50, 50, 100, 100, 1.0, 1.0, 1.0, dibujar);
+ * fuerza_bruta(50, 50, 200, 50, 1.0, 1.0, 1.0, dibujar);
+ * fuerza_bruta(200, 1, 200, 100, 1.0, 1.0, 1.0, dibujar);
+ * 
+ */
--- src/include/fuerza_bruta.h
+++ src/include/fuerza_bruta.h
@@ -1,5 +1,10 @@
 #ifndef FUERZA_BRUTRA_H
 #define FUERZA_BRUTRA_H
 
+#include <math.h>
+#include <stdio.h>
+
+void fuerza_bruta(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
+                void (*func_dibujar)(int,int,double,double,double));
 
 #endif
