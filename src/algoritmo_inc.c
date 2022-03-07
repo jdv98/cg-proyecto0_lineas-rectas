a348177004cfffa53b56e55badfc184924002983
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-07 08:59:31 -0600

    closes #2
    base para algoritmo incremental

--- src/algoritmo_inc.c
+++ src/algoritmo_inc.c
@@ -1,1 +1,17 @@
 #include "include/algoritmo_inc.h"
+
+void algoritmo_inc(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
+                   void (*func_dibujar)(int, int, double, double, double))
+{
+    long double m,y;
+
+    m=(y1-y0)/(x1-x0);
+    y=y0;
+
+    for (int i = x0; i < x1; i++)
+    {
+        (*func_dibujar)(i, (int)(round(y)), rc, gc, bc);
+        y+=m;
+    }
+    
+}
--- src/include/algoritmo_inc.h
+++ src/include/algoritmo_inc.h
@@ -1,5 +1,9 @@
 #ifndef ALGORITMO_INC_H
 #define ALGORITMO_INC_H
 
+#include <math.h>
+
+void algoritmo_inc(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
+                   void (*func_dibujar)(int, int, double, double, double));
 
 #endif
