172b227a8a0cc89fc2c3f08cb2e44258294630c2
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-10 09:00:54 -0600

    closes #3

--- src/algoritmo_inc_v2.c
+++ src/algoritmo_inc_v2.c
@@ -1,1 +1,27 @@
 #include "include/algoritmo_inc_v2.h"
+
+int max(int a, int b);
+
+void algoritmo_inc_v2(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
+                      void (*func_dibujar)(int, int, double, double, double))
+{
+    int width = max(abs(x1 - x0), abs(y1 - y0));
+
+    long double x = x0,
+                y = y0,
+                mx = (x1 - x0) / width,
+                my = (y1 - y0) / width;
+
+    for (int i = 0; i < width; i++)
+    {
+        (*func_dibujar)((int)(round(x)), (int)(round(y)), rc, gc, bc);
+        x += mx;
+        y += my;
+    }
+}
+
+int max(int a, int b)
+{
+    int *r = a > b ? &a : &b;
+    return r > 0 ? *r : 1;
+}
