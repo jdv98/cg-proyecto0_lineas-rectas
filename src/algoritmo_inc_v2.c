229ddf6eeac002bf04c1ed6cc3e8e3b16d5adc0b
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-10 12:45:23 -0600

    cambio en parametros

--- src/algoritmo_inc_v2.c
+++ src/algoritmo_inc_v2.c
@@ -2,8 +2,7 @@
 
 int max(int a, int b);
 
-void algoritmo_inc_v2(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
-                      void (*func_dibujar)(int, int, double, double, double))
+void algoritmo_inc_v2(int x0, int y0, int x1, int y1, COLOR * color,void (*func_dibujar)(int,int,COLOR *))
 {
     int width = max(abs(x1 - x0), abs(y1 - y0));
 
@@ -14,7 +13,7 @@ void algoritmo_inc_v2(int x0, int y0, int x1, int y1, double rc, double gc, doub
 
     for (int i = 0; i < width; i++)
     {
-        (*func_dibujar)((int)(round(x)), (int)(round(y)), rc, gc, bc);
+        (*func_dibujar)((int)(round(x)), (int)(round(y)), color);
         x += mx;
         y += my;
     }
--- src/include/algoritmo_inc_v2.h
+++ src/include/algoritmo_inc_v2.h
@@ -2,8 +2,9 @@
 #define ALGORITMO_INC_V2_H
 
 #include <math.h>
+#include "dibujar.h"
 
-void algoritmo_inc_v2(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
-                void (*func_dibujar)(int,int,double,double,double));
+void algoritmo_inc_v2(int x0, int y0, int x1, int y1, COLOR * color,
+                void (*func_dibujar)(int,int,COLOR *));
 
 #endif
