717834b35729c3b8008861df65ea5650a8016a5f
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-10 09:00:54 -0600

    closes #3

--- src/include/algoritmo_inc_v2.h
+++ src/include/algoritmo_inc_v2.h
@@ -1,5 +1,9 @@
 #ifndef ALGORITMO_INC_V2_H
 #define ALGORITMO_INC_V2_H
 
+#include <math.h>
+
+void algoritmo_inc_v2(int x0, int y0, int x1, int y1, double rc, double gc, double bc,
+                void (*func_dibujar)(int,int,double,double,double));
 
 #endif
