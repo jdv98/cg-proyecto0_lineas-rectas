1a8b086001c4b975717d9c253ca5f10a5e83a178
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-11 22:15:57 -0600

    base

--- src/algoritmo_bresenham.c
+++ src/algoritmo_bresenham.c
@@ -1,1 +1,16 @@
 #include "include/algoritmo_bresenham.h"
+
+void cuadrante1(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar)(int, int, COLOR *));
+void cuadrante2(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar)(int, int, COLOR *));
+
+void bresenham(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar)(int, int, COLOR *))
+{
+}
+
+void cuadrante1(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar)(int, int, COLOR *))
+{
+}
+
+void cuadrante2(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar)(int, int, COLOR *))
+{
+}
--- src/include/algoritmo_bresenham.h
+++ src/include/algoritmo_bresenham.h
@@ -1,5 +1,7 @@
 #ifndef ALGORITMO_BRESENHAM_H
 #define ALGORITMO_BRESENHAM_H
 
+void bresenham(int x0, int y0, int x1, int y1, COLOR * color,
+                void (*func_dibujar)(int,int,COLOR *));
 
 #endif
