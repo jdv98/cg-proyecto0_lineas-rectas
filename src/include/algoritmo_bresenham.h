d68ce659998fdbca4cdda85f46e5937b88495504
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-11 22:18:28 -0600

    includes

--- src/include/algoritmo_bresenham.h
+++ src/include/algoritmo_bresenham.h
@@ -1,6 +1,9 @@
 #ifndef ALGORITMO_BRESENHAM_H
 #define ALGORITMO_BRESENHAM_H
 
+#include <math.h>
+#include "dibujar.h"
+
 void bresenham(int x0, int y0, int x1, int y1, COLOR * color,
                 void (*func_dibujar)(int,int,COLOR *));
 
