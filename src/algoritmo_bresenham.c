1cb642d68975ce0a8140837e6da40c10a9618335
LuisGTO <luisgto33@gmail.com>
2022-03-12 23:42:03 -0600

    closes #4
    Algoritmo de don brese funcional

--- src/algoritmo_bresenham.c
+++ src/algoritmo_bresenham.c
@@ -38,22 +38,103 @@ void bresenham(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar
 
 void cuadrante1(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar)(int, int, COLOR *))
 {
-    delta_ne=2*(delta_y-delta_x);
-    if (delta_x > delta_y) // octante1
+    //
+    int d, xp, yp;
+    int Delta_E = 2 * (delta_y);
+    int Delta_NE = 2 * ((delta_y) - (delta_x));
+    int Delta_N = -2 * (delta_x);
+    //
+    xp = x0;
+    yp = y0;
+
+    d = 2*delta_y-delta_x;
+    //
+
+    if (delta_x > delta_y) // octante1 /5
     {
+        while (xp < x1)
+        {
+            (*func_dibujar)((xp), (yp), color);
+            if (d <= 0)
+            {
+                xp++;
+                d = d + Delta_E;
+            }
+            else
+            {
+                xp++;
+                yp++;
+                d = d + Delta_NE;
+            }
+        }
     }
-    else
+    else //               octante2 / 6
     {
+        while (yp < y1)
+        {
+            if (d > 0)
+            {
+                yp++;
+                d = d + Delta_N;
+            }
+            else
+            {
+                xp++;
+                yp++;
+                d = d + Delta_NE;
+            }
+            (*func_dibujar)((xp), (yp), color);
+        }
     }
 }
 
 void cuadrante2(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibujar)(int, int, COLOR *))
 {
-    delta_se=2*(delta_x + delta_y);
-    if ( > 0)
+    //
+    int d, xp, yp;
+    int Delta_E = 2 * delta_y;
+    int Delta_S = 2 * delta_x;
+    int Delta_SE = 2 * (delta_y + delta_x);
+    //
+    xp = x0;
+    yp = y0;
+    d = (2*delta_y) + delta_x;
+
+    if (delta_x > abs(delta_y)) // octante  8/4
     {
+        while (xp < x1)
+        {
+            if (d > 0)
+            {
+                xp++;
+                d = d + Delta_E;
+            }
+            else
+            {
+                xp++;
+                yp--;
+                d = d + Delta_SE;
+            }
+            (*func_dibujar)((xp), (yp), color);
+        }
+        return;
     }
-    else
+    else // Octante 7 / 3
     {
+        while (yp > y1)
+        {
+            (*func_dibujar)((xp), (yp), color);
+            if (d <= 0)
+            {
+                yp--;
+                d = d + Delta_S;
+            }
+            else
+            {
+                xp++;
+                yp--;
+                d = d + Delta_SE;
+            }
+        }
     }
 }
--- src/include/algoritmo_bresenham.h
+++ src/include/algoritmo_bresenham.h
@@ -2,6 +2,7 @@
 #define ALGORITMO_BRESENHAM_H
 
 #include <math.h>
+#include <stdlib.h>
 #include "dibujar.h"
 
 void bresenham(int x0, int y0, int x1, int y1, COLOR * color,
