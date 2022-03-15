8dcb249996abb41e4a7d6600a2b4a8ab449622d8
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2022-03-14 23:30:23 -0600

    cambio en condicionales closes #9

--- src/fuerza_bruta.c
+++ src/fuerza_bruta.c
@@ -15,18 +15,24 @@ void fuerza_bruta(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibu
         x1 = tmpx;
         y1 = tmpy;
     }
-    if (x0 == x1)
+
+    int x = (x1 - x0),
+        y = (y1 - y0);
+
+    int x0_y1 = (abs(x) > abs(y)) ? 0 : 1;
+
+    if (x==0)
     {
-        m = (long double)(y1 - y0);
+        m = (long double)y;
     }
     else
     {
-        m = (long double)(y1 - y0) / (x1 - x0);
+        m = (long double)y / x;
     }
 
     b = (long double)y0 - m * x0;
 
-    if (x0 == x1 && y0 != y1)
+    if (x0 == x1 && y0 < y1)
     {
         for (int i = y0; i <= y1; i++)
         {
@@ -40,15 +46,7 @@ void fuerza_bruta(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibu
             (*func_dibujar)(i, y0, color);
         }
     }
-    else if (b<0 && y0<y1)////Y
-    {
-        for (int i = y0; i <= y1; i++)
-        {
-            yx =(int)(round((i - b) / m));
-            (*func_dibujar)(yx,i, color);
-        }
-    }
-    else if ((b<0 && y1<y0) || (m<-1 && y1<y0)  )////Y
+    else if (x0_y1 && y0 > y1)////Y
     {
         for (int i = y1; i <= y0; i++)
         {
@@ -56,20 +54,19 @@ void fuerza_bruta(int x0, int y0, int x1, int y1, COLOR *color, void (*func_dibu
             (*func_dibujar)(yx,i, color);
         }
     }
-    else {////X
+    else if (x0_y1 && y0 < y1)////Y
+    {
+        for (int i = y0; i <= y1; i++)
+        {
+            yx =(int)(round((i - b) / m));
+            (*func_dibujar)(yx,i, color);
+        }
+    }
+    else if(!x0_y1) {////X
         for (int i = x0; i <= x1; i++)
         {
-            yx =(int)(round(m * i + b));
+            yx =(int)(floor(m * i + b));
             (*func_dibujar)(i, yx, color);
         }
     }
 }
-
-/**
- * Casos:
- *
- * fuerza_bruta(50, 50, 100, 100, 1.0, 1.0, 1.0, dibujar);
- * fuerza_bruta(50, 50, 200, 50, 1.0, 1.0, 1.0, dibujar);
- * fuerza_bruta(200, 1, 200, 100, 1.0, 1.0, 1.0, dibujar);
- *
- */
